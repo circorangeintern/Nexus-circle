1c52e4c47549bbf694bea17e9a09b6f99a64e2af
eniitanadeleke <eniitanadeleke@gmail.com>
2026-07-23 03:41:52 +0100

    Clear mobile-app pages, restructure for web version

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoadingScreen from './pages/LoadingScreen';
-import OnboardingA from './pages/OnboardingA';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<LoadingScreen />} />
-          <Route path="/onboardingA" element={<OnboardingA />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="app">
+      <h1>FloodAlert Web</h1>
+    </div>
   );
 }
 
